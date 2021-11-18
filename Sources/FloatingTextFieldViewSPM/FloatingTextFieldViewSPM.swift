97ad38b484043f54d2b82e2de340cb303d18e6d3
vasanth <vasanth.kamal@anywhere.co>
2021-11-18 12:49:19 +0530

    intial commit

---
+++ Sources/FloatingTextFieldViewSPM/FloatingTextFieldViewSPM.swift
@@ -0,0 +1,10 @@
+public struct FloatingTextFieldViewSPM {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func callNib() {
+        print("successfully call the nib...")
+    }
+}
---
+++ Tests/FloatingTextFieldViewSPMTests/FloatingTextFieldViewSPMTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import FloatingTextFieldViewSPM
+
+final class FloatingTextFieldViewSPMTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FloatingTextFieldViewSPM().text, "Hello, World!")
+    }
+}
